914173761b577b8d49d07e36186fd62d50264566
Nicolás García <nicolas.garcia.r@ug.uchile.cl>
2021-12-20 16:57:11 -0300

    Se intenta conectar a una base de datos en MongoDB

--- components/messages/store.js
+++ components/messages/store.js
@@ -1,11 +1,40 @@
-const list = [];        // Lista de mensajes
+const db = require('mongoose');
+
+// Conexion con base de datos
+// mongodb+srv://db_user:Lf5nu4QmTtUkUtc6@cluster0.ni0u5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+
+const { MongoClient } = require('mongodb');
+const uri = "mongodb+srv://db_user:Lf5nu4QmTtUkUtc6@cluster0.ni0u5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    console.log("Conectado a BBDD")
+    client.close();
+});
+
 
 function addMessage(message){
-    list.push(message);
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+
+        console.log("Conectado a BBDD")
+        const myMsj = new db.Model(message);
+        myMsj.save();
+
+        client.close();
+    });
 }
 
 function getMessages(){
-    return list;
+    client.connect(err => {
+        const collection = client.db("test").collection("devices");
+
+        console.log("Conectado a BBDD")
+        const messages = db.Model.find();
+
+        client.close();
+        return messages;
+    });
 }
 
 // Exportamos las funciones del almacenamiento
